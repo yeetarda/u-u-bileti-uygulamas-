405e5b10d80e631b3b9d2b93aba53b933da94a75
yeetarda <162746163+yeetarda@users.noreply.github.com>
2025-05-24 16:43:10 +0300

    Main.java

--- UÇUŞ_BİLETİ_UYGULAMASI/Main.java
+++ UÇUŞ_BİLETİ_UYGULAMASI/Main.java
@@ -7,40 +7,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n=== Uçuş Listesi ===");
+            System.out.println("\n=== UÇUŞ LİSTESİ ===");
             servis.ucuslariListele();
 
-            System.out.print("Rezervasyon yapmak istediğiniz uçuş numarasını girin (0 = çıkış): ");
+            System.out.print("rezervasyon yapmak istediğiniz uçuş numarasını girin (0 = çıkış): ");
             int secim = sc.nextInt();
             sc.nextLine();
 
             if (secim == 0) {
-                System.out.println("Çıkılıyor.");
+                System.out.println("çıkılıyor");
                 break;
             }
 
-            System.out.print("Adınız: ");
+            System.out.print("adınız: ");
             String ad = sc.nextLine();
 
-            System.out.print("Soyadınız: ");
+            System.out.print("soyadınız: ");
             String soyad = sc.nextLine();
 
-            System.out.print("Yaşınız: ");
+            System.out.print("yaşınız: ");
             int yas = sc.nextInt();
             sc.nextLine();
 
-            System.out.print("Telefon Numaranız: ");
+            System.out.print("telefon numaranız: ");
             String telefon = sc.nextLine();
 
-            System.out.print("TC Kimlik Numaranız: ");
+            System.out.print("TC kimlik numaranız: ");
             String tcKimlik = sc.nextLine();
-
             boolean basarili = servis.rezervasyonYap(secim, ad, soyad, yas, telefon, tcKimlik);
 
             if (basarili) {
-                System.out.println("Rezervasyon başarılı! Dosyaya kaydedildi.");
+                System.out.println("rezervasyon başarılı dosyaya kaydedildi");
             } else {
-                System.out.println("Rezervasyon başarısız. Uçakta yer yok veya giriş geçersiz.");
+                System.out.println("rezervasyon başarısız uçakta yer yok veya giriş geçersiz");
             }
         }
     }
